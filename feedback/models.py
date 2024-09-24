c117c0a033219f2ae74a9dc20d16a7afb06d4fef
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-24 23:14:41 +0500

    change

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class FeedbackModel(models.Model):
+    tittle = models.CharField(max_length=200)
+    message = models.TextField()
+    email = models.EmailField()
+    status = models.BooleanField(default=False)
+    user_firstname = models.CharField(max_length=200)
+    user_lastname = models.CharField(max_length=200)
+
+    created_at = models.DateTimeField(auto_now_add=True)
