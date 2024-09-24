e26ab398b5158dd180a233063c63d2e6890186ba
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-24 23:13:35 +0500

    change

--- servicing/models.py
+++ servicing/models.py
@@ -6,3 +6,6 @@ class ServiceModel(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='media/services')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
