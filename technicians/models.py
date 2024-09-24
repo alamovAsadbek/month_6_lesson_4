27ec2df0a06f056db1b7c23e70c40fad1d34d434
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-24 23:13:11 +0500

    change

--- technicians/models.py
+++ technicians/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class TechnicianModel(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
