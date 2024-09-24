c2739be3e9a01ca7d517aefa39f0c7302692d3ab
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-24 22:56:36 +0500

    add about_page_view func

--- servicing/views.py
+++ servicing/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # home page
 def home_page_view(request):
     return render(request, 'index.html')
+
+
+# about page
+def about_page_view(request):
+    return render(request, 'about.html')
