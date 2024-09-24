e896117d7153a52acc76c2c33b792332335ab7a5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-24 23:00:36 +0500

    change

--- servicing/urls.py
+++ servicing/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
-from servicing.views import home_page_view
+from servicing.views import home_page_view, about_page_view, contact_page_view, service_page_view, booking_page_view, \
+    team_page_view, testimonial_page_view
 
 urlpatterns = [
-    path('', home_page_view, name='home_page')
+    path('', home_page_view, name='home_page'),
+    path('about/', about_page_view, name='about_page'),
+    path('contact/', contact_page_view, name='contact_page'),
+    path('service/', service_page_view, name='service_page'),
+    path('booking/', booking_page_view, name='booking_page'),
+    path('team/', team_page_view, name='team_page'),
+    path('testimonial/', testimonial_page_view, name='testimonial_page'),
 ]
