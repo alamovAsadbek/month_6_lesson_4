f964290e6ae2311edb60c4f0ef5ee003b10be9a7
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-26 22:50:16 +0500

    change admin.py

--- servicing/admin.py
+++ servicing/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from servicing.models import ServiceModel
 
-admin.site.register(ServiceModel)
+
+@admin.register(ServiceModel)
+class ServiceModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description', 'name', 'price', 'description', 'created_at')
+    list_display_links = ('id', 'description', 'name', 'price', 'description', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('description', 'name', 'price')
+    list_per_page = 10
