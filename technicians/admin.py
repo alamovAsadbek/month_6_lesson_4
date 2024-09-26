c1b9212ba21559c4467dfad1a56963cee5ca3476
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-26 22:39:57 +0500

    change admin.py

--- technicians/admin.py
+++ technicians/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from technicians.models import TechnicianModel
 
-admin.site.register(TechnicianModel)
+
+@admin.register(TechnicianModel)
+class TechnicianAdmin(admin.ModelAdmin):
+    pass
