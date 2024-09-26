27fde408008ba69f59c2e44422b0a71768ccb650
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-26 22:44:33 +0500

    change admin.py4

--- technicians/admin.py
+++ technicians/admin.py
@@ -5,7 +5,8 @@ from technicians.models import TechnicianModel
 
 @admin.register(TechnicianModel)
 class TechnicianAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'address']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'address', 'created_at']
+    list_display_links = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'address', 'created_at']
     search_fields = ['first_name', 'last_name', 'email', 'phone_number', 'address']
     readonly_fields = ['id']
     fieldsets = [
