597ec4b194f7c7855fd9702f061a436380d74290
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-26 23:07:18 +0500

    change admin.oy

--- technicians/admin.py
+++ technicians/admin.py
@@ -8,8 +8,4 @@ class TechnicianAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'address', 'created_at']
     list_display_links = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'address', 'created_at']
     search_fields = ['first_name', 'last_name', 'email', 'phone_number', 'address']
-    fieldsets = [
-        ('Personal Information', {'fields': ['first_name', 'last_name', 'email', 'phone_number', 'address']}),
-        ('Technical Information', {'fields': ['id']}),
-    ]
     list_per_page = 10
