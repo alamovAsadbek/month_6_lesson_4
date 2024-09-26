a072c962859cc66c1510bef408c96629ec5194fc
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-26 22:26:58 +0500

    change

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'feedback',
+    'servicing',
+    'technicians'
 ]
 
 MIDDLEWARE = [
