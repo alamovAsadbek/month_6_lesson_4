898dc001ee0708b6bfa4811b5c76537d890a3aeb
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-26 23:12:16 +0500

    change admin.py

--- feedback/admin.py
+++ feedback/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from feedback.models import FeedbackModel
 
+admin.site.site_header = 'Admin Panel'
+
 
 @admin.register(FeedbackModel)
 class FeedbackAdmin(admin.ModelAdmin):
