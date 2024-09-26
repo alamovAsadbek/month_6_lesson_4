4b1f4cfec844b9360edc6a99d48ea2c38dc7665f
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-26 23:06:47 +0500

    change admin.py

--- technicians/admin.py
+++ technicians/admin.py
@@ -8,7 +8,6 @@ class TechnicianAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'address', 'created_at']
     list_display_links = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'address', 'created_at']
     search_fields = ['first_name', 'last_name', 'email', 'phone_number', 'address']
-    readonly_fields = ['id']
     fieldsets = [
         ('Personal Information', {'fields': ['first_name', 'last_name', 'email', 'phone_number', 'address']}),
         ('Technical Information', {'fields': ['id']}),
