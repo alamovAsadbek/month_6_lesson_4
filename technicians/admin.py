02055aa541ea9c13ed09298a90480a4f61f3b608
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-26 22:43:35 +0500

    change admin.py

--- technicians/admin.py
+++ technicians/admin.py
@@ -7,3 +7,9 @@ from technicians.models import TechnicianModel
 class TechnicianAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'address']
     search_fields = ['first_name', 'last_name', 'email', 'phone_number', 'address']
+    readonly_fields = ['id']
+    fieldsets = [
+        ('Personal Information', {'fields': ['first_name', 'last_name', 'email', 'phone_number', 'address']}),
+        ('Technical Information', {'fields': ['id']}),
+    ]
+    list_per_page = 10
