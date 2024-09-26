19d1b0ca8cb1ca68936eb511ac9f1b817b9fbffa
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-26 22:41:59 +0500

    changa admin.py

--- technicians/admin.py
+++ technicians/admin.py
@@ -5,4 +5,5 @@ from technicians.models import TechnicianModel
 
 @admin.register(TechnicianModel)
 class TechnicianAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'email', 'phone_number', 'address']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'address']
+    search_fields = ['first_name', 'last_name', 'email', 'phone_number', 'address']
